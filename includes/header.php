7acd97534c6eb249672507aef251788c3de3c0b6
YC <ycgoh04@gmail.com>
2025-03-02 17:09:56 +0800

    Design Index.php
    - designed on header.php which includes navigation bar

---
+++ includes/header.php
@@ -0,0 +1,51 @@
+<header class="header">
+    <nav class="navbar">
+        <!-- Hamburger Menu Icon -->
+        <i class="ri-menu-line hamburger-icon" aria-label="Menu"></i>
+
+        <!-- Logo -->
+        <div class="logo">TUB</div>
+
+        <!-- Navigation Links -->
+        <ul class="nav-menu">
+            <li><a href="index.php">Home</a></li>
+
+            <li class="dropdown">
+                <a href="#">Mens <i class="ri-arrow-down-s-line"></i></a>
+                <ul class="dropdown-menu">
+                    <li><a href="#">Shirts</a></li>
+                    <li><a href="#">Pants</a></li>
+                    <li><a href="#">Shoes</a></li>
+                </ul>
+            </li>
+            <li class="dropdown">
+                <a href="#">Womens <i class="ri-arrow-down-s-line"></i></a>
+                <ul class="dropdown-menu">
+                    <li><a href="#">Dresses</a></li>
+                    <li><a href="#">Bags</a></li>
+                    <li><a href="#">Accessories</a></li>
+                </ul>
+            </li>
+            <li class="dropdown">
+                <a href="#">Blog <i class="ri-arrow-down-s-line"></i></a>
+                <ul class="dropdown-menu">
+                    <li><a href="#">Fashion Tips</a></li>
+                    <li><a href="#">Trends</a></li>
+                </ul>
+            </li>
+            <li class="dropdown">
+                <a href="#">About <i class="ri-arrow-down-s-line"></i></a>
+                <ul class="dropdown-menu">
+                    <li><a href="#">Our Story</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </li>
+        </ul>
+
+        <div class="nav-icons">
+            <i class="ri-search-line"></i>
+            <i class="ri-user-line"></i>
+            <i class="ri-shopping-bag-line"></i>
+        </div>
+    </nav>
+</header>
