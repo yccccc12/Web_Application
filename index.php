26d9263b5a89b496dc96fa01f95a719e9e12311b
jsw <jswong0819@gmail.com>
2025-03-10 11:14:26 +0800

    image reinstall
    aaa

---
+++ index.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="en">
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TUB Concept Store</title>
+
+    <!-- Google Font: Figtree -->
+    <link href="https://fonts.googleapis.com/css2?family=Figtree:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    
+    <!-- REMIXICONS -->
+    <link rel="stylesheet" href="style/styles.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/remixicon/3.5.0/remixicon.css">
+
+</head>
+<body>
+    <?php include 'includes/header.php';?>
+
+    <section class="landing-section">
+        <h1>Style Without Limits</h1>
+        <p>
+            Welcome to TUB – where fashion meets confidence, and style has no limits. We create bold, trendsetting pieces designed for those who dare to stand out. 
+            Explore the latest drops, discover timeless essentials, and redefine your wardrobe with us. Because fashion isn’t just what you wear – it’s how you own it.
+        </p>
+    </section>
+
+    <div name="landing-image-container">
+        <img name="landing-image" src="img/landing.webp" alt="This is an image of models">
+    </div>
+
+    <section class="new-arrivals">
+        <h2>New Arrivals</h2>
+        <section class="products-section">
+            <div class="product">
+                <img src="img/item1.jpg" alt="Men Lightweight Hoodies Jacket">
+                <h3>Men Lightweight Hoodies Jacket</h3>
+                <p>RM199.90</p>
+            </div>
+            <div class="product">
+                <img src="img/item2.jpg" alt="Men Oversized T-Shirt Short">
+                <h3>Men Oversized T-Shirt Short</h3>
+                <p>RM159.90</p>
+            </div>
+            <div class="product">
+                <img src="img/item3.jpg" alt="Men Bomber Jacket">
+                <h3>Men Bomber Jacket</h3>
+                <p>RM219.90</p>
+            </div>
+            <div class="product">
+                <img src="img/item4.jpg" alt="Men Oversized T-Shirt Short">
+                <h3>Men Oversized T-Shirt Short</h3>
+                <p>RM159.90</p>
+            </div>
+            <div class="product">
+                <img src="img/item5.jpg" alt="Women Long-Sleeve Sweatshirt">
+                <h3>Women Long-Sleeve Sweatshirt</h3>
+                <p>RM119.90</p>
+            </div>
+            <div class="product">
+                <img src="img/item6.jpg" alt="Women Oversized Denim Jacket">
+                <h3>Women Oversized Denim Jacket</h3>
+                <p>RM169.90</p>
+            </div>
+            <div class="product">
+                <img src="img/item7.jpg" alt="Women Crop Polo Tee">
+                <h3>Women Crop Polo Tee</h3>
+                <p>RM109.90</p>
+            </div>
+            <div class="product">
+                <img src="img/item8.jpg" alt="Women Polo Tee">
+                <h3>Women Polo Tee</h3>
+                <p>RM89.90</p>
+            </div>
+        </section>
+        <button class="view-all-button">View all</button>
+    </section>
+
+    <div name="landing-image-container">
+        <img name="landing-image" src="img/landing2.jpg" alt="This is an image of models">
+    </div>
+
+    <section class="why-choose-section">
+        <div class="reason-description">
+            <h2>Why Choose Us?</h2>
+
+            <h3>Shop with Confidence</h3>
+            <p>
+                Find the perfect fit for your wardrobe with our easy-to-navigate collections.
+            </p>
+
+            <h3>Affordable and Stylish</h3>
+            <p>
+                We offer high-quality fashion at prices that won’t break the bank.
+            </p>
+
+            <h3>Fresh Styles for You</h3>
+            <p>
+                Discover our latest collection of trendy and comfortable clothing. Perfect for any occasion.
+            </p>
+        </div>
+        <div name="why-choose-image-container">
+            <img name="why-choose-image" src="img/landing3.jpg" alt="Fashion Banner">
+        </div>
+    </section>
+
+    <?php include 'includes/footer.php';?>
+</body>
+</html>
